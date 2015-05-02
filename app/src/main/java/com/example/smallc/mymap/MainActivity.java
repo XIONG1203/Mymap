e0533990d729f8ba96357f616696fbbe771e81c3
XIONG1203 <xiongjiachen@outlook.com>
2015-05-02 16:34:26 +0800

    bullshit

--- app/src/main/java/com/example/smallc/mymap/MainActivity.java
+++ app/src/main/java/com/example/smallc/mymap/MainActivity.java
@@ -1,43 +1,87 @@
 package com.example.smallc.mymap;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Bundle;
 import android.util.Log;
+import android.view.View.OnClickListener;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationConfiguration.LocationMode;
 import com.baidu.mapapi.map.BaiduMap;
-import com.baidu.mapapi.map.BaiduMapOptions;
-import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
 
 
 public class MainActivity extends Activity {
-
-    BaiduMap baiduMap;
-    MapView mapView=null;
-    private String location_latitude;
-    private String  location_longitude;
-    private BroadcastReceiver myBroadcastReceiver=new MyBroadcastReceiver();
-    private TextView textView;
-
-
+    BaiduMap mBaiduMap;
+    MapView mMapview;
+    LocationClient mLocationClient;
+    Button mButton;
+    private LocationMode locationMode;
+    boolean isFirstLoc=true;
+    BitmapDescriptor bitmapDescriptor;
+    MyLocationListener myLocationListener=new MyLocationListener();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-        Intent serviceIntent=new Intent(this,MyIntentService.class);
-        startService(serviceIntent);
-        textView=(TextView)findViewById(R.id.tv);
+        mButton=(Button)findViewById(R.id.btn1);
+        locationMode= LocationMode.NORMAL;
+
+        OnClickListener onClickListener=new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (locationMode){
+                   case NORMAL:
+                       mButton.setText("跟随");
+                       locationMode=LocationMode.FOLLOWING;
+                       mBaiduMap.setMyLocationConfigeration(
+                               new MyLocationConfiguration(locationMode,true,bitmapDescriptor));
+                       break;
+                   case FOLLOWING:
+                       mButton.setText("罗盘");
+                       locationMode=LocationMode.COMPASS;
+                       mBaiduMap.setMyLocationConfigeration(
+                               new MyLocationConfiguration(locationMode,true,bitmapDescriptor));
+                       break;
+                   case COMPASS:
+                       mButton.setText("普通");
+                       locationMode=LocationMode.NORMAL;
+                       mBaiduMap.setMyLocationConfigeration(
+                               new MyLocationConfiguration(locationMode,true,bitmapDescriptor));
+                       break;
+                }
+            }
+        };
+        mButton.setOnClickListener(onClickListener);
+
+        mMapview=(MapView)findViewById(R.id.bmapView);
+        mBaiduMap=mMapview.getMap();
+        mBaiduMap.setMyLocationEnabled(true);
+
+        mLocationClient=new LocationClient(this);
+        mLocationClient.registerLocationListener(myLocationListener);
+        LocationClientOption option=new LocationClientOption();
+        option.setOpenGps(true);
+        option.setCoorType("bd09ll");
+        option.setScanSpan(1000);
+        mLocationClient.setLocOption(option);
+        mLocationClient.start();
 
     }
 
@@ -65,35 +109,51 @@ public class MainActivity extends Activity {
     }
 
 
-    @Override
     protected void onResume() {
+        mMapview.onResume();
         super.onResume();
-        IntentFilter filter = new IntentFilter();
-        filter.addAction("com.example.broadcasttest.MY_BROADCAST");
-        this.registerReceiver(myBroadcastReceiver, filter);
     }
 
-    @Override
+
     protected void onPause() {
+        mMapview.onPause();
         super.onPause();
     }
 
     protected void onStop() {
-        // TODO Auto-generated method stub
         super.onStop();
-        this.unregisterReceiver(myBroadcastReceiver);
     }
 
     @Override
     protected void onDestroy() {
+        mMapview.onDestroy();
+        mMapview=null;
+        mLocationClient.stop();
+        mBaiduMap.setMyLocationEnabled(false);
         super.onDestroy();
     }
 
-    class MyBroadcastReceiver extends BroadcastReceiver {
+     class MyLocationListener implements BDLocationListener{
         @Override
-        public void onReceive(Context context, Intent intent) {
-            location_latitude=intent.getStringExtra("LOCATION_LATITUDE");
-            location_longitude=intent.getStringExtra("LOCATION_LONGITUDE");
+        public void onReceiveLocation(BDLocation location) {
+            if(location==null||mMapview==null){
+                return;
+            }
+
+            MyLocationData locationData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    .direction(100).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locationData);
+
+            if(isFirstLoc){
+                isFirstLoc=false;
+                Log.d("MainActivity","aaaaa");
+                LatLng latLng=new LatLng(location.getLatitude(),location.getLongitude());
+                MapStatusUpdate mapStatusUpdate= MapStatusUpdateFactory.newLatLng(latLng);
+                mBaiduMap.animateMapStatus(mapStatusUpdate);
+            }
         }
+
     }
 }
